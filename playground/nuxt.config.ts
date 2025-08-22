1b78209220797512167bca9e30b1ee6eff2da85d
Yegor Zakharov <yegor.zakharov99@gmail.com>
2025-08-23 01:26:01 +0200

    make module build - maybe crap fix

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,9 +3,11 @@ export default defineNuxtConfig({
     '../src/module',
     '@nuxt/ui',
   ],
+  rootDir: '.',
   edgeDb: {
     auth: true,
     oauth: true,
+    dbschemaDir: 'dbschema/',
   },
   compatibilityDate: '2025-08-23',
   devtools: { enabled: true },
